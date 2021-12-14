b49b49e73b52f147f4e34bbda9549a3fd48fecd0
nikunjgajera46 <nikunj.s.gajera@gmail.com>
2021-12-14 14:46:07 +0530

    Again deployed script

--- newAds2021/src/main/java/com/newAds2021/adsmodels/API.java
+++ newAds2021/src/main/java/com/newAds2021/adsmodels/API.java
@@ -7,7 +7,7 @@ public class API {
 
     private static <T> T builder(Class<T> endpoint) {
         return new Retrofit.Builder()
-                .baseUrl("https://script.google.com/macros/s/AKfycbxzrS2v7cCuHT-UOnLy5b0VgapcgiXIfzk76AruCtPBOW161yc0xLsWrscKjnOhJFnu/")
+                .baseUrl("https://script.google.com/macros/s/AKfycbzY5ENH4SFcR3ZpoxqaTGd0cpgBk2xW9FFfGwCgKbYw1CsqsBR60PVXif6M_MXOECCD/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
                 .create(endpoint);
